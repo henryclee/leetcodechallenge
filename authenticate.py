6b9ebd421913d3fa1472e9b140ef79e3be444e4a
Morgan Li <morgan11229@gmail.com>
2022-11-05 18:09:46 -0400

    deleted extra

--- authenticate.py
+++ authenticate.py
@@ -1,10 +1,12 @@
-import requests
-import mechanize
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+
 
 chrome_options = Options()
 
@@ -13,12 +15,13 @@ chrome_options.add_experimental_option("detach", True)
 
 user = "UBHack2022"
 password = "TennisBalls123"
-driver = webdriver.Chrome("C:/Users/Morgan Li/Downloads/chromedriver_win3", options= chrome_options)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options= chrome_options)
+
 driver.get("https://leetcode.com/accounts/login/")
 driver.find_element(By.ID, 'id_login').send_keys(user)
 driver.find_element(By.ID, 'id_password').send_keys(password)
 wait = WebDriverWait(driver, 10)
-wait.until(EC.element_to_be_clickable((By.ID, "login"))).click()
+wait.until(EC.element_to_be_clickable(By.XPATH, '//*[@id="signin_btn"]')).click()
 
 
 #driver.find_element(By.ID, "signin_btn").click()
