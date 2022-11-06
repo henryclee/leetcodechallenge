1173d77b04a3997d816d0055d5657458b769f550
ben <73725152+bentan1020@users.noreply.github.com>
2022-11-05 22:42:50 -0400

    connect 2-3

--- app.py
+++ app.py
@@ -3,23 +3,21 @@ import authenticate
 
 app = Flask(__name__)
 
+arr = []
+
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        cookies = authenticate(username, password)
-        return redirect('/')
-        """
         try:
-            authenticate(username, password)
-            print(authenticate(username, password))
+            authenticate.authenticate(username, password)
+            arr = authenticate.authenticate(username, password)
             return redirect('/')
         except:
             return "There was an issue"
-        """
     else:
-        return render_template('index.html', cookies=cookies)
+        return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, threaded=True)
