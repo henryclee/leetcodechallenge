f65b500fcc528429d012ebebd14243d71d9e3d39
Henry Lee <henryclee73@gmail.com>
2022-11-05 20:10:02 -0400

    updated insertuser, getLeaders, and working on getNewProblem

---
+++ getLeaders.py
@@ -0,0 +1,28 @@
+import mysql.connector
+
+def insertproblem(name: str, problem: str):
+
+    config = {
+        'user': 'hackathon',
+        'password': 'hack',
+        'host': '127.0.0.1',
+        'database': 'codingchallenge',
+        'raise_on_warnings': True
+    }
+
+    cnx = mysql.connector.connect(**config)
+
+    cursor = cnx.cursor()
+
+    cursor.execute("SELECT leetcodeName, number_solved FROM usertable ORDER BY number_solved descending LIMIT 100")
+
+    leaders = [(str,int)]
+
+    for leader in cursor:
+        leaders.append(leader)
+    
+    cnx.commit()
+    cursor.close()
+    cnx.close()
+
+    return leaders
